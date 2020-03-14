da91d74b7fe71b939cdc4d3fc31df828de3af3ec
udonba <mitsuki.bando@gmail.com>
2020-03-15 01:59:18 +0900

    1st commit

---
+++ OculusShareDownloader/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OculusShareDownloader
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("TEXT", "TITLE", MessageBoxButton.OKCancel, MessageBoxImage.Information) == MessageBoxResult.OK)
+            {
+                Cmd();
+            }
+        }
+
+        private void Cmd()
+        {
+            //Processオブジェクトを作成
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+
+            //ComSpec(cmd.exe)のパスを取得して、FileNameプロパティに指定
+            p.StartInfo.FileName = "adb.exe";
+
+
+            //出力を読み取れるようにする
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = false;
+            //ウィンドウを表示しないようにする
+            //p.StartInfo.CreateNoWindow = true;
+            //コマンドラインを指定（"/c"は実行後閉じるために必要）
+            p.StartInfo.Arguments = @"devices";
+
+            //起動
+            p.Start();
+
+            //出力を読み取る
+            string results = p.StandardOutput.ReadToEnd();
+
+            //プロセス終了まで待機する
+            //WaitForExitはReadToEndの後である必要がある
+            //(親プロセス、子プロセスでブロック防止のため)
+            p.WaitForExit();
+            p.Close();
+
+            //出力された結果を表示
+            Console.WriteLine(results);
+        }
+
+        private void Test()
+        {
+            Process.Start("adb", "devices");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Cmd();
+        }
+    }
+}
