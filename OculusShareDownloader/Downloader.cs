c7ece9dcdc93df775fd6f08c19574d16fd371f77
udonba <mitsuki.bando@gmail.com>
2020-03-15 17:29:54 +0900

    基本機能追加
    - デバイスIDの取得
    - Screenshots, Videoshots一覧の取得
    - typo修正

---
+++ OculusShareDownloader/Downloader.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static OculusShareDownloader.Extension;
+
+namespace OculusShareDownloader
+{
+    public class Downloader
+    {
+        public const string PATH_SCREENSHOTS = "/sdcard/oculus/screenshots/";
+        public const string PATH_VIDEOSHOTS = "/sdcard/oculus/videoshots/";
+
+        public static string GetCmdOutput(string exe, string arg)
+        {
+#if DEBUG
+            Console.WriteLine(string.Format(@"""{0} {1}""", exe, arg));
+#endif
+            Process p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = false;
+            p.StartInfo.CreateNoWindow = true;
+
+            p.StartInfo.FileName = exe;
+            p.StartInfo.Arguments = arg;
+            p.Start();
+
+            string results = p.StandardOutput.ReadToEnd();
+
+            p.WaitForExit();
+            p.Close();
+
+            return results;
+        }
+
+        public static string[] GetDevices()
+        {
+            var names = new List<string>();
+            var lines = GetCmdOutput("adb", "devices").SplitToLine();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (TryParseDeviceName(lines[i], out string name))
+                {
+                    names.Add(name);
+                }
+            }
+            return names.ToArray();
+        }
+
+        public static string[] GetFiles(string directory, string deviceName = "")
+        {
+            string arg = "shell ls " + directory;
+            if (deviceName != "")
+                arg = string.Format("-s {0} {1}", deviceName, arg);
+
+            var lines = GetCmdOutput("adb", arg).SplitToLine();
+            var paths = new string[lines.Length];
+            if (!directory.EndsWith("/"))
+                directory += "/";
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                paths[i] = directory + lines[i];
+            }
+            return paths;
+        }
+    }
+}
--- OculusShareDownloader/MainWindow.xaml.cs
+++ OculusShareDownloader/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static OculusShareDownloader.Extension;
 
 namespace OculusShareDownloader
 {
@@ -26,56 +27,48 @@ namespace OculusShareDownloader
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            if (MessageBox.Show("TEXT", "TITLE", MessageBoxButton.OKCancel, MessageBoxImage.Information) == MessageBoxResult.OK)
+            var name = GetDeviceNameTest();
+            if (name != "")
             {
-                Cmd();
+                GetScreenshotsTest(name);
+                GetVideoshotsTest(name);
             }
         }
 
-        private void Cmd()
+        private string GetDeviceNameTest()
         {
-            //Processオブジェクトを作成
-            System.Diagnostics.Process p = new System.Diagnostics.Process();
-
-            //ComSpec(cmd.exe)のパスを取得して、FileNameプロパティに指定
-            p.StartInfo.FileName = "adb.exe";
-
-
-            //出力を読み取れるようにする
-            p.StartInfo.UseShellExecute = false;
-            p.StartInfo.RedirectStandardOutput = true;
-            p.StartInfo.RedirectStandardInput = false;
-            //ウィンドウを表示しないようにする
-            //p.StartInfo.CreateNoWindow = true;
-            //コマンドラインを指定（"/c"は実行後閉じるために必要）
-            p.StartInfo.Arguments = @"devices";
-
-            //起動
-            p.Start();
-
-            //出力を読み取る
-            string results = p.StandardOutput.ReadToEnd();
-
-            //プロセス終了まで待機する
-            //WaitForExitはReadToEndの後である必要がある
-            //(親プロセス、子プロセスでブロック防止のため)
-            p.WaitForExit();
-            p.Close();
-
-            //出力された結果を表示
-            Console.WriteLine(results);
+            Console.WriteLine("Get device names...");
+            var lines = Downloader.GetDevices();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
+            if (lines.Length != 0)
+                return lines[0];
+            else
+                return "";
         }
 
-        private void Test()
+        private void GetScreenshotsTest(string deviceName = "")
         {
-            Process.Start("adb", "devices");
+            Console.WriteLine("Get screenshots...");
+            var lines = Downloader.GetFiles(Downloader.PATH_SCREENSHOTS, deviceName);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
+        }
+        private void GetVideoshotsTest(string deviceName = "")
+        {
+            Console.WriteLine("Get videoshots...");
+            var lines = Downloader.GetFiles(Downloader.PATH_VIDEOSHOTS, deviceName);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            Cmd();
-        }
     }
 }
